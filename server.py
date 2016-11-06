54fd0dcfa97f72629e3deca40bfd3db93501b661
Laury Bueno <laury.bueno@gmail.com>
2016-11-06 00:29:42 -0200

    App python adaptado para rodar na nuvem Bluemix com Flask

--- server.py
+++ server.py
@@ -1,10 +1,5 @@
 import os
-try:
-    from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-    from SocketServer import TCPServer as Server
-except ImportError:
-    from http.server import SimpleHTTPRequestHandler as Handler
-    from http.server import HTTPServer as Server
+from flask import Flask, request, send_from_directory
 
 # {
 #   "url": "https://gateway.watsonplatform.net/conversation/api",
@@ -12,15 +7,26 @@ except ImportError:
 #   "username": "6382d1d9-ebbe-4637-aac3-79f340dd009c"
 # }
 
-# Read port selected by the cloud for our application
-PORT = int(os.getenv('PORT', 8000))
-# Change current directory to avoid exposure of control files
-os.chdir('static')
+app = Flask(__name__)
 
-httpd = Server(("", PORT), Handler)
-try:
-    print("Start serving at port %i" % PORT)
-    httpd.serve_forever()
-except KeyboardInterrupt:
-    pass
-httpd.server_close()
+
+@app.route('/stylesheets/<path:path>')
+def send_css(path):
+    return send_from_directory('static/stylesheets', path)
+
+
+@app.route('/images/<path:path>')
+def send_img(path):
+    return send_from_directory('static/images', path)
+
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+
+if __name__ == '__main__':
+    # Bind to PORT/HOST if defined, otherwise default to 5050/localhost.
+    PORT = int(os.getenv('VCAP_APP_PORT', 8000))
+    # HOST = str(os.getenv('VCAP_APP_HOST', 'localhost'))
+    app.run(host='0.0.0.0', port=PORT)
---
+++ setup.py
@@ -0,0 +1,24 @@
+"""
+Hello World app for running Python apps on Bluemix
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#     long_description = f.read()
+
+setup(
+    name='consultor-compras',
+    version='1.0.0',
+    description='descrição',
+    long_description="descrição longa",
+    url='',
+    license='Apache-2.0'
+)
